f1714a7b355800355894478bc6f402967b93f6f5
Amartya Mondal <hayyoulistentome@gmail.com>
2020-05-03 19:29:06 +0530

    final commit

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,7 +1,7 @@
 import 'story.dart';
 
 class StoryBrain {
-  int storyNumber = 0;
+  int _storyNumber = 0;
   List<Story> _storyData = [
     Story(
         storyTitle:
@@ -35,40 +35,44 @@ class StoryBrain {
   ];
 
   String getStory() {
-    return _storyData.first.storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
   String getChoice1() {
-    return _storyData.first.choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
   String getChoice2() {
-    return _storyData.first.choice2;
+    return _storyData[_storyNumber].choice2;
+  }
+
+  void restart() {
+    _storyNumber = 0;
   }
 
   void nextStory(int choiceNumber) {
-    if (storyNumber == 0 && choiceNumber == 1) {
-      storyNumber = 2;
-    } else if (storyNumber == 0 && choiceNumber == 2) {
-      storyNumber = 1;
-    } else if (storyNumber == 1 && choiceNumber == 2) {
-      storyNumber = 3;
-    } else if (storyNumber == 1 && choiceNumber == 1) {
-      storyNumber = 2;
-    } else if (storyNumber == 2 && choiceNumber == 1) {
-      storyNumber = 5;
-    } else if (storyNumber == 2 && choiceNumber == 2) {
-      storyNumber = 4;
+    if (_storyNumber == 0 && choiceNumber == 1) {
+      _storyNumber = 2;
+    } else if (_storyNumber == 0 && choiceNumber == 2) {
+      _storyNumber = 1;
+    } else if (_storyNumber == 1 && choiceNumber == 2) {
+      _storyNumber = 3;
+    } else if (_storyNumber == 1 && choiceNumber == 1) {
+      _storyNumber = 2;
+    } else if (_storyNumber == 2 && choiceNumber == 1) {
+      _storyNumber = 5;
+    } else if (_storyNumber == 2 && choiceNumber == 2) {
+      _storyNumber = 4;
+    } else {
+      restart();
+    }
+  }
+
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
-
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
